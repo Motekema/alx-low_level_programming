e0ab312fde3d65f03fc577e4644621831cf6dbe8
Motekema <motekema@gmail.com>
2023-06-15 10:10:21 -0700

    My commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,16 +5,15 @@
  *
  * Description: using sizeof to print the size of various types.
  *
- * Return: Always 0 (Succesful)
+ * Return: Always 0 (Successful)
  */
 
 int main(void)
 {
-
 /* sizeof evaluates the size of a variable */
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of an int: %zu byte(s)\n", sizeof(int));
-printf("Size of an double: %zu byte(s)\n", sizeof(double));
+printf("Size of a double: %zu byte(s)\n", sizeof(double));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %zu byte(s)\n", sizeof(float));
