306a6e221974004bf816ffc474b8e0c8e5f42768
Motekema <motekema@gmail.com>
2023-06-15 05:58:28 -0700

    My commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -10,6 +9,6 @@
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art, \n");
 return (0);
 }
