989b6c7eb1c40b00ac3e0e20c53e9e02e01ef04d
Motekema <motekema@gmail.com>
2023-08-01 00:04:40 -0700

    My commit

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,8 +1,10 @@
 #include "lists.h"
 
 /**
-* free_listint - frees a linked list
-*/
+ * free_listint - frees a linked list
+ * @head: listint_t list to be freed
+ */
+
 void free_listint(listint_t *head)
 {
 listint_t *temp;
