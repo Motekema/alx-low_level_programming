2624d10351d3140468d3f8b9f4b2df3319828027
Motekema <motekema@gmail.com>
2023-07-31 23:59:59 -0700

    My commit

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,19 +1,20 @@
 #include "lists.h"
 
 /**
- * returns the number of elements in a linked lists
+ * listint_len - returns the number of elements in a linked lists
+ * @h: linked list of type listint_t to traverse
  *
  * Return: number of nodes
  */
 size_t listint_len(const listint_t *h)
 {
-size_t num1 = 0;
+size_t num = 0;
 
 while (h)
 {
-num1++;
+num++;
 h = h->next;
 }
 
-return (num1);
+return (num);
 }
