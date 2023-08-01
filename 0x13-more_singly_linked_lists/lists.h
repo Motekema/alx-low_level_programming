13f8fd19085e67b313a98a3e74da678e603a5848
Motekema <motekema@gmail.com>
2023-07-31 23:41:11 -0700

    My commit

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -10,13 +10,12 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *
+ * for alx project
  */
-
 typedef struct listint_s
 {
-int n;
-struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -36,3 +35,4 @@ size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
 #endif
+
