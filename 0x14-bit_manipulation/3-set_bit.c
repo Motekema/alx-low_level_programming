e77bae390f1ab3c27fe5ccb78636d5b9a69b4e8e
Motekema <motekema@gmail.com>
2023-08-03 03:28:20 -0700

    My commit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,11 +7,11 @@
  *
  * Return: 1 for success, -1 for failure
  */
-int set_bit(unsigned long int *n, unsigned int index1)
+int set_bit(unsigned long int *n, unsigned int index)
 {
-if (index1 > 63)
+if (index > 63)
 return (-1);
 
-*n = ((1UL << index1) | *n);
+*n = ((1UL << index) | *n);
 return (1);
 }
