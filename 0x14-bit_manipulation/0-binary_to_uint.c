804883be2be70da0cc3cd01db7e1f7362a64cdff
Motekema <motekema@gmail.com>
2023-08-03 03:06:46 -0700

    My commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,7 +11,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 int i;
-unsigned int dec_value = 0;
+unsigned int dec_val = 0;
 
 if (!b)
 return (0);
@@ -20,8 +20,8 @@ for (i = 0; b[i]; i++)
 {
 if (b[i] < '0' || b[i] > '1')
 return (0);
-dec_value = 2 * dec_value + (b[i] - '0');
+dec_val = 2 * dec_val + (b[i] - '0');
 }
 
-return (dec_value);
+return (dec_val);
 }
