7eb0c411a5a9bf4a442db259cf7474919a59ef69
Motekema <motekema@gmail.com>
2023-08-07 05:47:54 -0700

    My commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,59 +4,55 @@
 #include <unistd.h>
 
 /**
- * create_data_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
- *
- * Return: A pointer to the newly-allocated buffer.
- */
+* create_buffer - Allocates 1024 bytes for a buffer.
+* @file: The name of the file buffer is storing chars for.
+*
+* Return: A pointer to the newly-allocated buffer.
+*/
 
-
-char *create_data_buffer(char *file);
+char *create_buffer(char *file);
 void close_file(int fd);
 
-char *create_data_buffer(char *file) {
-char *buffer = malloc(sizeof(char) * 1024);
-if (buffer == NULL) {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
-exit(99);
-}
-return (buffer);
-}
-
-void close_file(int fd) {
-if (close(fd) == -1) {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-exit(100);
-}
+void error_exit(const char *message) {
+dprintf(STDERR_FILENO, "%s\n", message);
+exit(1);
 }
 
 int main(int argc, char *argv[]) {
+char *buffer;
+int from, to;
+ssize_t bytes_read, bytes_written;
+
 if (argc != 3) {
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
+error_exit("Usage: cp file_from file_to");
 }
 
-char *buffer = create_data_buffer(argv[2]);
-int from = open(argv[1], O_RDONLY), to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-ssize_t r, w;
+buffer = create_buffer(argv[2]);
 
-do {
-if ((r = read(from, buffer, 1024)) == -1) {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-free(buffer);
-exit(98);
+from = open(argv[1], O_RDONLY);
+if (from == -1) {
+error_exit("Error: Can't read from file");
 }
 
-if ((w = write(to, buffer, r)) == -1) {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-free(buffer);
-exit(99);
+to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+if (to == -1) {
+error_exit("Error: Can't write to file");
 }
-} while (r > 0);
 
-free(buffer);
-close_file(from);
-close_file(to);
+while ((bytes_read = read(from, buffer, sizeof(buffer))) > 0) {
+bytes_written = write(to, buffer, bytes_read);
+if (bytes_written == -1) {
+error_exit("Error: Can't write to file");
+}
+}
+
+if (bytes_read == -1) {
+error_exit("Error: Can't read from file");
+}
+
+if (close(from) == -1 || close(to) == -1) {
+error_exit("Error: Can't close file descriptor");
+}
 
 return (0);
 }
