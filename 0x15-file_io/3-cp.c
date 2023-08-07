6512b847ef6d2521f7991b7ac93b8f6abf46dce4
Motekema <motekema@gmail.com>
2023-08-07 05:56:28 -0700

    My commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,56 +3,61 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-/**
-* create_buffer - Allocates 1024 bytes for a buffer.
-* @file: The name of the file buffer is storing chars for.
-*
-* Return: A pointer to the newly-allocated buffer.
-*/
-
-char *create_buffer(char *file);
-void close_file(int fd);
-
 void error_exit(const char *message) {
 dprintf(STDERR_FILENO, "%s\n", message);
 exit(1);
 }
 
-int main(int argc, char *argv[]) {
-char *buffer;
-int from, to;
-ssize_t bytes_read, bytes_written;
-
-if (argc != 3) {
-error_exit("Usage: cp file_from file_to");
+char *create_buffer(char *file) {
+char *buffer = malloc(1024);
+if (buffer == NULL) {
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+exit(99);
 }
+return buffer;
+}
+
+void close_file(int fd) {
+if (close(fd) == -1) {
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+exit(100);
+}
+}
+
+int main(int argc, char *argv[]) {
+int from, to;
+ssize_t r, w;
+char *buffer;
+
+if (argc != 3) error_exit("Usage: cp file_from file_to");
 
 buffer = create_buffer(argv[2]);
-
 from = open(argv[1], O_RDONLY);
-if (from == -1) {
-error_exit("Error: Can't read from file");
+r = read(from, buffer, 1024);
+to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+do {
+if (from == -1 || r == -1) {
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+free(buffer);
+exit(98);
 }
 
-to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0644);
-if (to == -1) {
-error_exit("Error: Can't write to file");
+w = write(to, buffer, r);
+if (to == -1 || w == -1) {
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+free(buffer);
+exit(99);
 }
 
-while ((bytes_read = read(from, buffer, sizeof(buffer))) > 0) {
-bytes_written = write(to, buffer, bytes_read);
-if (bytes_written == -1) {
-error_exit("Error: Can't write to file");
-}
-}
+r = read(from, buffer, 1024);
+to = open(argv[2], O_WRONLY | O_APPEND);
 
-if (bytes_read == -1) {
-error_exit("Error: Can't read from file");
-}
+} while (r > 0);
 
-if (close(from) == -1 || close(to) == -1) {
-error_exit("Error: Can't close file descriptor");
-}
+free(buffer);
+close_file(from);
+close_file(to);
 
 return (0);
 }
