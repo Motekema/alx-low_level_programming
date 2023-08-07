1bcd03399454a45112ef20ecdf671b5cde6e6b22
Motekema <motekema@gmail.com>
2023-08-07 05:17:58 -0700

    My commit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -4,7 +4,7 @@
 * append_text_to_file - Appends text at the end of a file.
 * @filename: A pointer to the name of the file.
 * @text_content: The string to add to the end of the file.
-* Return: 1 on success and -1 on failure or filename is NULL - -1. 
+* Return: 1 on success and -1 on failure or filename is NULL - -1.
 */
 
 int append_text_to_file(const char *filename, char *text_content)
