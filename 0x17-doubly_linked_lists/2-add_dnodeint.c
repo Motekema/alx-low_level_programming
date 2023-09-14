9d069e38fd7ac4d2c047a540070f0fea1e09f8b6
Motekema <motekema@gmail.com>
2023-09-14 09:30:06 -0700

    My commit

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,37 +1,31 @@
 #include "lists.h"
 
 /**
-* add_dnodeint - Add a new node at the beginning of a dlistint_t list
-* @head: Pointer to the pointer of the head of the list
-* @n: The integer value to be stored in the new node
-*
-* Return: The address of the new element, or NULL if it failed
-*/
+ * add_dnodeint - Add a new node at the beginning of a dlistint_t list
+ * @head: Pointer to the pointer of the head of the list
+ * @n: The integer value to be stored in the new node
+ *
+ * Return: The address of the new element, or NULL if it failed
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-dlistint_t *new_node;
-dlistint_t *z;
+    dlistint_t *new_node;
 
-new_node = malloc(sizeof(dlistint_t));
-if (new_node == NULL)
-return (NULL);
+    if (head == NULL)
+        return (NULL);
 
-new_node->n = n;
-new_node->prev = NULL;
-z = *head;
+    new_node = malloc(sizeof(dlistint_t));
+    if (new_node == NULL)
+        return (NULL);
 
-if (z != NULL)
-{
-while (h->prev != NULL)
-h = h->prev;
-}
+    new_node->n = n;
+    new_node->prev = NULL;
+    new_node->next = *head;
 
-new_node->next = z;
+    if (*head != NULL)
+        (*head)->prev = new_node;
 
-if (z != NULL)
-z->prev = new_node;
+    *head = new_node;
 
-*head = new_node;
-
-return (new_node);
+    return (new_node);
 }
--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,38 +1,39 @@
 #include "lists.h"
 
 /**
-* add_dnodeint_end - Add a new node at the end of a dlistint_t list
-* @head: Pointer to the pointer of the head of the list
-* @n: The integer value to be stored in the new node
-*
-* Return: The address of the new element, or NULL if it failed
-*/
+ * add_dnodeint_end - Add a new node at the end of a dlistint_t list
+ * @head: Pointer to the pointer of the head of the list
+ * @n: The integer value to be stored in the new node
+ *
+ * Return: The address of the new element, or NULL if it failed
+ */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-dlistint_t *z;
-dlistint_t *new_node;
+    dlistint_t *new_node;
+    dlistint_t *temp = *head;
 
-new_node = malloc(sizeof(dlistint_t));
-if (new_node == NULL)
-return (NULL);
+    if (head == NULL)
+        return (NULL);
 
-new_node->n = n;
-new_node->next = NULL;
+    new_node = malloc(sizeof(dlistint_t));
+    if (new_node == NULL)
+        return (NULL);
 
-z = *head;
+    new_node->n = n;
+    new_node->prev = NULL;
+    new_node->next = NULL;
 
-if (z != NULL)
-{
-while (z->next != NULL)
-z = z->next;
-h->next = new_node;
-}
-else
-{
-*head = new_node;
-}
+    if (*head == NULL)
+    {
+        *head = new_node;
+        return (new_node);
+    }
 
-new_node->prev = z;
+    while (temp->next != NULL)
+        temp = temp->next;
 
-return (new_node);
+    temp->next = new_node;
+    new_node->prev = temp;
+
+    return (new_node);
 }
