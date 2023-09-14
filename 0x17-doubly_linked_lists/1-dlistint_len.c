6d012d1e404882628f64fcf5d5fa292c37d7842d
Motekema <motekema@gmail.com>
2023-09-14 09:20:07 -0700

    My commit

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,20 +1,37 @@
 #include "lists.h"
 
 /**
- * dlistint_len - Return the number of elements in a dlistint_t list
- * @h: Pointer to the head of the list
- *
- * Return: The number of nodes in the list
- */
+* dlistint_len - Return the number of elements in a dlistint_t list
+* @h: Pointer to the head of the list
+*
+* Return: The number of nodes in the list
+*/
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t count = 0;
-    
-    while (h != NULL)
-    {
-        h = h->next;
-        count++;
-    }
-    
-    return (count);
+dlistint_t *z;
+dlistint_t *new_node;
+
+new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+return (NULL);
+
+new_node->n = n;
+new_node->next = NULL;
+
+z = *head;
+
+if (z != NULL)
+{
+while (z->next != NULL)
+z = z->next;
+z->next = new_node;
+}
+else
+{
+*head = new_node;
+}
+
+new_node->prev = z;
+
+return (new_node);
 }
--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,39 +1,38 @@
 #include "lists.h"
 
 /**
- * add_dnodeint_end - Add a new node at the end of a dlistint_t list
- * @head: Pointer to the pointer of the head of the list
- * @n: The integer value to be stored in the new node
- *
- * Return: The address of the new element, or NULL if it failed
- */
+* add_dnodeint_end - Add a new node at the end of a dlistint_t list
+* @head: Pointer to the pointer of the head of the list
+* @n: The integer value to be stored in the new node
+*
+* Return: The address of the new element, or NULL if it failed
+*/
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node;
-    dlistint_t *temp = *head;
+dlistint_t *z;
+dlistint_t *new_node;
 
-    if (head == NULL)
-        return (NULL);
+new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+return (NULL);
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return (NULL);
+new_node->n = n;
+new_node->next = NULL;
 
-    new_node->n = n;
-    new_node->prev = NULL;
-    new_node->next = NULL;
+z = *head;
 
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return (new_node);
-    }
+if (z != NULL)
+{
+while (z->next != NULL)
+z = z->next;
+h->next = new_node;
+}
+else
+{
+*head = new_node;
+}
 
-    while (temp->next != NULL)
-        temp = temp->next;
+new_node->prev = z;
 
-    temp->next = new_node;
-    new_node->prev = temp;
-
-    return (new_node);
+return (new_node);
 }
