d0e29ad25e58d58307b11655bc282e131f373e01
Motekema <motekema@gmail.com>
2023-09-19 08:17:37 -0700

    My commit

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int add(int a, int b) 
+{
+	return a + b;
+}
+
+
+int mul(int a, int b) 
+{
+        return a * b;
+}
+
+int sub(int a, int b) 
+{
+	return a - b;
+}
+
+int div(int a, int b) 
+{
+	if (b == 0) 
+	{
+		fprintf(stderr, "Error: Division error\n");
+		return 0;
+	}
+	return a / b;
+}
+
+int mod(int a, int b) 
+{
+	if (b == 0) 
+	{
+		fprintf(stderr, "Error: Division error\n");
+		return 0;
+	}
+	return a % b;
+}
+
