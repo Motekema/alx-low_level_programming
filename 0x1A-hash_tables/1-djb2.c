43c82266ea75469ea747a738821667dff626f81a
Motekema <motekema@gmail.com>
2023-10-06 05:41:41 -0700

    My commit

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,36 +1,21 @@
-unsigned long int hash_djb2(const unsigned char *str)
-{
-	    unsigned long int hash;
-	        int c;
-
-		    hash = 5381;
-		        while ((c = *str++))
-				    {
-					            hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-						        }
-			    return (hash);
-}
-julien@ubuntu:~/0x1A. Hash tables$ 
-julien@ubuntu:~/0x1A. Hash tables$ cat 1-main.c 
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "hash_tables.h"
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always EXIT_SUCCESS.
- *     */
-int main(void)
+* hash_djb2 - function that generates random number through
+* a string given (hash numbers)
+*
+* @str: strings
+* Return: hash numbers
+*/
+unsigned long int hash_djb2(const unsigned char *str)
 {
-	    char *s;
+unsigned long int hash_;
+int f;
 
-	        s = "cisfun";
-		    printf("%lu\n", hash_djb2((unsigned char *)s));
-		        s = "Don't forget to tweet today";
-			    printf("%lu\n", hash_djb2((unsigned char *)s));
-			        s = "98";
-				    printf("%lu\n", hash_djb2((unsigned char *)s));
-				        return (EXIT_SUCCESS);
+hash_ = 5381;
+while ((f = *str++))
+{
+hash_ = ((hash_ << 5) + hash_) +f; /* hash_ * 33 + f */
+}
+return (hash_);
 }
